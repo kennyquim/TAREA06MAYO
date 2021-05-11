9302778a35f8a7302432aacb86e6772fd5bd5839
kennyquim <kennyqb89@gmail.com>
2021-05-11 16:27:46 -0500

    Actividad en casa en JavaScript

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,10 @@
-function imprimir(){
- for (let i = 2; i < 22; i++, i++) {
-    let table = 
-    `<tr>
+for (let i = 2; i < 22; i++, i++) {
+  let table = `<tr>
         <th scope="row"> ${i}</th>
         <td>Marcos</td>
         <td>Medellin</td>
         <td>3875698421</td>
       </tr>`;
-    
-    document.getElementById("tabla_valores").innerHTML += table;
-  }
+
+  document.getElementById("tabla_valores").innerHTML += table;
 }
